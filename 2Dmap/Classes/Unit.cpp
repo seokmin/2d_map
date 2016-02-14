b3f5fe448f15d02054e5ea0f43b54a2dc8324495
Sorae <seokmin@nhnnext.org>
2016-02-14 21:24:03 +0900

    1

---
+++ 2Dmap/Classes/Unit.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "Unit.h"
+
+
+Unit::Unit()
+{
+	_sprite = cocos2d::Sprite::create();
+}
+
+
+Unit::~Unit()
+{
+}
---
+++ 2Dmap/Classes/Unit.h
@@ -0,0 +1,10 @@
+#pragma once
+class Unit : cocos2d::Ref
+{
+public:
+	Unit();
+	virtual ~Unit();
+private:
+	cocos2d::Sprite*	_sprite;
+	
+};
